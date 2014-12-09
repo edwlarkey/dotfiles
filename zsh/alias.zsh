229b505ef7aceac0c9d66e3f640f79393074cbe2
Edward <edward@builderdesigns.com>
2014-12-09 08:56:04 -0600

    adding in a tmux configuration

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -37,10 +37,3 @@ fi
 # Show/hide hidden files in Finder
 alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
-# RDP
-alias rdp23='rdesktop -d smartcorp.net -u edlarkey -b -g 1440x900 10.10.1.23'
-alias rdp30='rdesktop -d smartcorp.net -u edlarkey -b -g 1440x900 10.10.1.30'
-alias luke='rdesktop -d hpdcs.local -u edwlarkey -b -g 1440x900 10.14.240.22'
-alias leia='rdesktop -d hpdcs.local -u edwlarkey -b -g 1440x900 10.14.240.21'
-alias rdp='rdesktop -d hpdcs.local -u edwlarkey -b -g 1440x900'
