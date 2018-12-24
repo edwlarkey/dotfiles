7266b9e5ab1075181e9031c9a3ac3dff0592062c
Edward Larkey <edwlarkey@mac.com>
2018-12-24 17:11:40 -0600

    Moving around git config, adding fzf aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -20,6 +20,18 @@ function aws-switch() {
     esac
 }
 
+function in() {
+  echo -e "$text\n\n" >> "$HOME/txt/vimwiki/inbox.md"
+}
+
+function ril() {
+  echo "- [ ] $*" >> "$HOME/txt/vimwiki/ReadingList.md"
+}
+
+function track() {
+  echo "- $*" >> "$HOME/txt/vimwiki/diary/$(date +'%Y-%m-%d').md"
+}
+
 ## Convenience and Safety
 if [[ ${OSTYPE} == linux* ]]; then
   alias rm='rm -I'
@@ -86,4 +98,102 @@ else
   else
     alias ls='ls -G'
   fi
+fi
+
+#
+# FZF
+# Most taken from https://github.com/junegunn/fzf/wiki/Examples
+#
+
+if (( $+commands[fzf] )) ; then
+  # fe [FUZZY PATTERN] - Open the selected file with the default editor
+  #   - Bypass fuzzy finder if there's only one match (--select-1)
+  #   - Exit if there's no match (--exit-0)
+  fe() {
+    local files
+    IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+    [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+  }
+
+  j() {
+    if [[ "$#" -ne 0 ]]; then
+        cd $(autojump $@)
+        return
+    fi
+    cd "$(autojump -s | sed '/_____/Q; s/^[0-9,.:]*\s*//' |  fzf --height 40% --reverse --inline-info)" 
+  }
+
+  # vf - fuzzy open with vim from anywhere
+  # ex: vf word1 word2 ... (even part of a file name)
+  # zsh autoload function
+  vf() {
+    local files
+
+    files=(${(f)"$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf --read0 -0 -1 -m)"})
+
+    if [[ -n $files ]]
+    then
+       vim -- $files
+       print -l $files[1]
+    fi
+  }
+
+  # vg - fuzzy open with vim with ag
+  # ex: vg search_string
+  # zsh autoload function
+  vg() {
+    local file
+
+    file="$(ag --nobreak --noheading $@ | fzf -0 -1 | awk -F: '{print $1 " +" $2}')"
+
+    if [[ -n $file ]]
+    then
+       vim $file
+    fi
+  }
+
+  # fd - cd to selected directory
+  fd() {
+    local dir
+    dir=$(find ${1:-.} -path '*/\.*' -prune \
+      -o -type d -print 2> /dev/null | fzf +m) &&
+    cd "$dir"
+  }
+
+  # cf - fuzzy cd from anywhere
+  # ex: cf word1 word2 ... (even part of a file name)
+  # zsh autoload function
+  cf() {
+    local file
+
+    file="$(locate $@ | grep --null -vE '~$' | fzf --read0 -0 -1)"
+
+    if [[ -n $file ]]
+    then
+       if [[ -d $file ]]
+       then
+          cd -- $file
+       else
+          cd -- ${file:h}
+       fi
+    fi
+  }
+
+  # fh - repeat history
+  fh() {
+    print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+  }
+
+  # tm - create new tmux session, or switch to existing one. Works from within tmux too. (@bag-man)
+  # `tm` will allow you to select your tmux session via fzf.
+  # `tm irc` will attach to the irc session (if it exists), else it will create it.
+
+  tm() {
+    [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+    if [ $1 ]; then
+      tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+    fi
+    session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+  }
+
 fi
