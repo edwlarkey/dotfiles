740880110becf19c6c296de08a501f132fd8a754
Edward Larkey <edwlarkey@mac.com>
2020-06-20 10:09:42 -0500

    git config and rm tmux fzf

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -187,16 +187,4 @@ if (( $+commands[fzf] )) ; then
     print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
   }
 
-  # tm - create new tmux session, or switch to existing one. Works from within tmux too. (@bag-man)
-  # `tm` will allow you to select your tmux session via fzf.
-  # `tm irc` will attach to the irc session (if it exists), else it will create it.
-
-  tm() {
-    [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-    if [ $1 ]; then
-      tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
-    fi
-    session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
-  }
-
 fi
