4f188c86ca3934504d970c6499947c3f72bcf9b2
Edward Larkey <edward.larkey@robinpowered.com>
2021-11-16 15:22:01 -0600

    FZF updates and ale completion

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -198,4 +198,29 @@ if (( $+commands[fzf] )) ; then
     print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
   }
 
+  #
+  # Git
+  #
+
+  # git commit browser
+  fshow() {
+    git log --graph --color=always \
+        --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+    fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+        --bind "ctrl-m:execute:
+                  (grep -o '[a-f0-9]\{7\}' | head -1 |
+                  xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                  {}
+  FZF-EOF"
+  }
+
+  # fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+  fbr() {
+    local branches branch
+    branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+    branch=$(echo "$branches" |
+             fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+  }
+
 fi
