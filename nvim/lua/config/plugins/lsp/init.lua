7a13162252d9248951cd7bb981345ad9beeb61aa
Edward Larkey <edward.larkey@robinpowered.com>
2023-01-06 08:33:58 -0600

    Fix schema and use lualine

--- nvim/lua/config/plugins/lsp/init.lua
+++ nvim/lua/config/plugins/lsp/init.lua
@@ -39,7 +39,7 @@ function M.config()
             {
               description = "Robin-deploy json config files",
               fileMatch = { "services/*/*.json" },
-              url = "https://gist.githubusercontent.com/edwlarkey/05dbe2b75bab01e52edb60b26f248315/raw/657da2b57726c840def7379fc8f80423fd067eaf/data.schema.json",
+              url = "/Users/edwardlarkey/git/robin/sysops/robin-deploy/services/data.schema.json",
             },
           },
         },
