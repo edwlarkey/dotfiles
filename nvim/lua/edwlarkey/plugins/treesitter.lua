14fe55fe65eed5ce7cfcada8ff8852ac27faee33
Edward Larkey <edwlarkey@mac.com>
2023-12-27 14:34:04 -0600

    add descriptions

--- nvim/lua/edwlarkey/plugins/treesitter.lua
+++ nvim/lua/edwlarkey/plugins/treesitter.lua
@@ -48,12 +48,12 @@ return {
         lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
         keymaps = {
           -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+          ["aa"] = { query = "@parameter.outer", desc = "around parameter" },
+          ["ia"] = { query = "@parameter.inner", desc = "inside parameter" },
+          ["af"] = { query = "@function.outer", desc = "around function" },
+          ["if"] = { query = "@function.inner", desc = "inside function" },
+          ["ac"] = { query = "@class.outer", desc = "around class" },
+          ["ic"] = { query = "@class.inner", desc = "inside clas" },
         },
       },
       move = {
