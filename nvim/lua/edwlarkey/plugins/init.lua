5fd76386ee27c0222a61d0c6e760c25750df8987
Edward Larkey <edward.larkey@robinpowered.com>
2024-09-12 14:06:12 +0000

    Add folke/persistence

--- nvim/lua/edwlarkey/plugins/init.lua
+++ nvim/lua/edwlarkey/plugins/init.lua
@@ -446,15 +446,39 @@ return {
     end,
   },
   {
-    "https://github.com/apple/pkl-neovim",
-    lazy = true,
-    event = "BufReadPre *.pkl",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    config = true,
+    keys = {
+      {
+        "<leader>qs",
+        function()
+          require("persistence").load()
+        end,
+        desc = "Restore Session",
+      },
+      {
+        "<leader>qS",
+        function()
+          require("persistence").select()
+        end,
+        desc = "Select Session",
+      },
+      {
+        "<leader>ql",
+        function()
+          require("persistence").load({ last = true })
+        end,
+        desc = "Restore Last Session",
+      },
+      {
+        "<leader>qd",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Don't Save Current Session",
+      },
     },
-    build = function()
-      vim.cmd("TSInstall! pkl")
-    end,
   },
   {
     "nvim-neotest/neotest",
