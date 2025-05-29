eb9497501fd71483431d98f5445f4461ba968d56
Edward Larkey <edward.larkey@robinpowered.com>
2025-05-29 16:03:14 +0000

    lsp updates

--- nvim/lua/edwlarkey/lsp.lua
+++ nvim/lua/edwlarkey/lsp.lua
@@ -1,15 +1,3 @@
-vim.lsp.enable({
-  "gopls",
-  "luals",
-  "golangci-lint-langserver",
-  "yaml-language-server",
-  "basedpyright",
-  "ruff",
-  "terraform_ls",
-  "dockerls",
-  "jsonls",
-})
-
 vim.diagnostic.config({
   virtual_lines = { current_line = true },
 })
@@ -19,3 +7,30 @@ local Util = require("edwlarkey.util")
 Util.on_attach(function(client, buffer)
   require("edwlarkey.keymaps").setup.lsp(buffer)
 end)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = au,
+    desc = 'LSP notify',
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client then
+            Snacks.notify(('attached to buffer %i'):format(args.buf), {
+                level = vim.log.levels.DEBUG,
+                title = 'LSP: ' .. client.name,
+            })
+        end
+    end,
+})
+
+vim.lsp.enable({
+  "gopls",
+  "luals",
+  "golangci-lint-langserver",
+  "yaml-language-server",
+  "basedpyright",
+  "ruff",
+  "terraform-ls",
+  "dockerls",
+  "jsonls",
+})
+
