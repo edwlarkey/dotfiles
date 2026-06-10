ebd468384f83fbfc62550496bf5d289cfc9bc69e
Edward Larkey <edwlarkey@mac.com>
2026-06-09 20:27:59 -0500

    Changes from kompot

--- nvim/plugin/01_plugins.lua
+++ nvim/plugin/01_plugins.lua
@@ -3,7 +3,7 @@ vim.pack.add({
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/nvim-lua/plenary.nvim" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
   { src = "https://github.com/ellisonleao/gruvbox.nvim" },
   { src = "https://github.com/Verf/deepwhite.nvim" },
   { src = "https://github.com/folke/snacks.nvim", lazy = false },
